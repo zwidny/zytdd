7f31cdd95c330db1b378afbb51e12d55516129b8
zwidny <zwidny@debian.zwidny>
2015-07-12 10:40:55 +0800

    Basic view now returns minimal HTML

--- src/functional_test.py
+++ src/functional_test.py
@@ -13,7 +13,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000/lists/')
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
--- src/lists/tests.py
+++ src/lists/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from .views import home
+
 
 
 # class SmokeTest(TestCase):
@@ -14,3 +17,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/lists/')
         self.assertEqual(found.func, home)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b"<title>To-Do lists</title>", response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
