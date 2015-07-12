846534984c24064dffad2d3729a18897ce9b802f
zwidny <zwidny@debian.zwidny>
2015-07-12 15:28:35 +0800

    " pass"

--- src/lists/tests.py
+++ src/lists/tests.py
@@ -31,3 +31,18 @@ class HomePageTest(TestCase):
         # good way
         expected_html = render_to_string('lists/home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST['item_text'] = "A new list item"
+
+        response = home(request)
+        self.assertIn("A new list item", response.content.decode())
+        expected_html = render_to_string(
+            'lists/home.html',
+            {
+                'new_item_text': 'A new list item'
+            }
+        )
+        self.assertEqual(response.content.decode(), expected_html)
--- src/lists/views.py
+++ src/lists/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'lists/home.html')
+    return render(request,
+                  'lists/home.html',
+                  {
+                    'new_item_text': request.POST.get("item_text", ""),
+                  })
