f05f9e1957f62742ce1933fc347851fe778f4fda
zwidny <zwidny@debian.zwidny>
2015-07-12 13:28:10 +0800

    Refactor home page view to use a template

--- src/lists/tests.py
+++ src/lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from .views import home
 
@@ -21,6 +22,12 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b"<title>To-Do lists</title>", response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+
+        # bad way
+        # self.assertTrue(response.content.strip().startswith(b'<html>'))
+        # self.assertIn(b"<title>To-Do lists</title>", response.content)
+        # self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+        # good way
+        expected_html = render_to_string('lists/home.html')
+        self.assertEqual(response.content.decode(), expected_html)
--- src/lists/views.py
+++ src/lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'lists/home.html')
