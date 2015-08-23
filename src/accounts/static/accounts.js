58613209bb94bcd744f31e4847d3c34a9040978a
zwidny <649038269@qq.com>
2015-08-23 11:47:08 +0800

    Finished chapter 15

--- src/accounts/static/accounts.js
+++ src/accounts/static/accounts.js
@@ -1,6 +1,17 @@
-var initialize = function(navigator){
+var initialize = function(navigator, user, token, urls){
   $('#id_login').on('click', function(){
     navigator.id.request();
+  });
+  navigator.id.watch({
+    loggedInUser: user,
+    onlogin: function(assertion){
+      $.post(urls.login,
+            {assertion: assertion, csrfmiddlewaretoken: token}
+      )
+      .fail(function(){navigator.id.logout();})
+      .done(function(){window.location.reload();});
+    },
+    onlogout: function(){},
   });
 };
 
