8f772fcd8eba8a696384d15513e1b2981afb2a64
zwidny <649038269@qq.com>
2015-08-25 10:19:41 +0800

    add get_user for accounts authentication models

--- src/accounts/tests/test_authentication.py
+++ src/accounts/tests/test_authentication.py
@@ -46,3 +46,26 @@ class AuthenticateTest(TestCase):
         mock_post.return_value.json.return_value = {}
         user = self.backend.authenticate("an assertion")
         self.assertIsNone(user)
+
+    def test_create_new_user_if_necessary_for_valid_assertion(self, mock_post):
+        mock_post.return_value.json.return_value = {'status': 'okay',
+                                                    'email': 'a@b.com'}
+        found_user = self.backend.authenticate("an assertion")
+        new_user = User.objects.get(email="a@b.com")
+        self.assertEqual(found_user, new_user)
+
+
+class GetUserTest(TestCase):
+
+    def test_gets_user_by_email(self):
+        backend = PersonaAuthenticationBackend()
+        other_user = User(email="other@user.com")
+        other_user.username = 'otheruser'
+        other_user.save()
+        desired_user = User.objects.create(email="a@b.com")
+        found_user = backend.get_user("a@b.com")
+        self.assertEqual(found_user, desired_user)
+
+    def test_returns_none_if_no_user_what_that_email(self):
+        backend = PersonaAuthenticationBackend()
+        self.assertIsNone(backend.get_user("a@b.com"))
