033a5c59734217bd2e05df841167572530fc5180
zwidny <649038269@qq.com>
2015-08-26 17:42:24 +0800

    placeholder teset_my_lists and move login checkers into base

--- src/functional_tests/test_login.py
+++ src/functional_tests/test_login.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
 from .base import FunctionalTest
-from selenium.webdriver.support.ui import WebDriverWait
+TEST_EMAIL = "edith@mockmyid.com"
 
 
 class LoginTest(FunctionalTest):
@@ -17,24 +17,6 @@ class LoginTest(FunctionalTest):
             time.sleep(0.5)
         self.fail("could not find window")
 
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id),
-            "Could not find element with id {}. Page text was {}".format(
-                element_id, self.browser.find_element_by_tag_name('body').text
-            )
-        )
-
-    def wait_to_be_logged_in(self):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn("edith@mockmyid.com", navbar.text)
-
-    def wait_to_be_logged_out(self):
-        self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn("edith@mockmyid.com", navbar.text)
-
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
@@ -48,24 +30,24 @@ class LoginTest(FunctionalTest):
         ## Use qq.com for test email
         self.browser.find_element_by_id(
             'authentication_email'
-        ).send_keys('edith@mockmyid.com')
+        ).send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
 
         # The Persona window closes
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         # Refreshing the page, she sees it's a real session login,
         # not just a one-off for that page
         self.browser.refresh()
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         # Terrified of this new feature, she reflexively clicks "logout"
         self.browser.find_element_by_id('id_logout').click()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
         # "logged out" status also persists after a refresh
         self.browser.refresh()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
